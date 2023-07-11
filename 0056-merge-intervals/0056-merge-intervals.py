3bf76551cc7b8c608b339ce9c349a3cbf170268d
Shankar Jonnavaram <58230306+shankarjonnavram@users.noreply.github.com>
2023-07-11 23:48:47 +0530

    Time: 165 ms (44.96%), Space: 21.1 MB (52.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,17 +4,21 @@ class Solution:
         new_lt = intervals[0]
         res_lt = []
         for i in range(1,len(intervals)):
-            print(new_lt)
-            if(new_lt[1]<intervals[i][0]):
-                res_lt.append(new_lt)
-                new_lt = intervals[i]
-            elif (new_lt[0]>intervals[i][1]):
-                res_lt.append(new_lt)
-                new_lt = intervals[i]
-            else:
+            # if(new_lt[1]<intervals[i][0]):
+            #     res_lt.append(new_lt)
+            #     new_lt = intervals[i]
+            # elif (new_lt[0]>intervals[i][1]):
+            #     res_lt.append(new_lt)
+            #     new_lt = intervals[i]
+            # else:
+            #     new_lt = [min(new_lt[0],intervals[i][0]),
+            #               max(new_lt[1],intervals[i][1])]
+            if(new_lt[1]>=intervals[i][0]):
                 new_lt = [min(new_lt[0],intervals[i][0]),
-                          max(new_lt[1],intervals[i][1])]
-            print(new_lt)
+                           max(new_lt[1],intervals[i][1])]
+            else:
+                res_lt.append(new_lt)
+                new_lt = intervals[i]
         res_lt.append(new_lt)
         return res_lt
             
