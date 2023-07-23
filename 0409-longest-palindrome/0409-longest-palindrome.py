9d6e6eb4aeddcfe7540d2951e198b164266c4930
Shankar Jonnavaram <58230306+shankarjonnavram@users.noreply.github.com>
2023-07-23 10:50:01 +0530

    Time: 48 ms (66.99%), Space: 16.4 MB (9.39%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,17 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        dt = {}
+        # dt = {}
+        # for i in s:
+        #     if(i in dt):
+        #         dt[i]+=1
+        #     else:
+        #         dt[i]=1
+        # s1 = 0
+        # n1 = 0
+        # for key in dt:
+        #     if(n1==0 and dt[key]%2!=0):
+        #         n1=(dt[key]//2)*2 + dt[key]%2
+        #     else:
+        #         s1+=dt[key]//2
+        # return s1*2+n1
+        
+        st = set()
+        # To get all characters which are present only once
         for i in s:
-            if(i in dt):
-                dt[i]+=1
+            if i not in st:
+                st.add(i)
             else:
-                dt[i]=1
-        s1 = 0
-        n1 = 0
-        for key in dt:
-            if(n1==0 and dt[key]%2!=0):
-                n1=(dt[key]//2)*2 + dt[key]%2
-            else:
-                s1+=dt[key]//2
-        return s1*2+n1
-            # print(f'key is {key} and value is {dt[key]}')
+                st.remove(i)
+        # Then checking the length of set.If Frequency is one we need to remove that count and add 1 (for odd length) .If len is 0 then everything is a palindrome
+        if(len(st)!=0):
+            return len(s)-len(st)+1
+        else:
+            return len(s)
