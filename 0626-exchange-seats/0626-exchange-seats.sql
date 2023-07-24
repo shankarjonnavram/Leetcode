531e71e2d0352e881a8ea52b9b68d910154accb9
Shankar Jonnavaram <58230306+shankarjonnavram@users.noreply.github.com>
2023-07-24 15:41:07 +0530

    Time: 799 ms (49.96%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
 
-with temp as(
-select id , student , 
+# with temp as(
+select 
 case
 when id%2=0 then id-1 
 when id=(select count(id) from seat) then id
 else id+1
-end as new 
-from seat
-)
+end as id , student 
+from seat order by 1
+# )
 
-select new as id , student from temp order by 1 
+# select new as id , student from temp order by 1 
 
-# select count(id) from seat
+# # select count(id) from seat
