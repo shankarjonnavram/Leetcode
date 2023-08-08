585f86492ac7cb895b56d08179f91320521195d6
Shankar Jonnavaram <58230306+shankarjonnavram@users.noreply.github.com>
2023-08-08 22:22:44 +0530

    Time: 529 ms (96.84%), Space: 33.3 MB (99.92%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -7,22 +7,44 @@ class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         if(head.next==None):
             return True
+        # ct =0
+        # lt = []
+        # while(fast.next!=None and fast.next.next!=None):
+        #     lt.append(slow.val)
+        #     slow = slow.next
+        #     fast = fast.next.next
+        #     ct+=1
+        # if(fast.next!=None):
+        #     lt.append(slow.val)
+        #     ct+=1
+        # while(slow.next!=None):
+        #         slow = slow.next
+        #         if(slow.val!=lt[ct-1]):
+        #             return False
+        #         ct-=1
+        # return True
         slow , fast = head , head
-        ct =0
-        lt = []
+        prev , new = None , None
         while(fast.next!=None and fast.next.next!=None):
-            lt.append(slow.val)
-            slow = slow.next
             fast = fast.next.next
-            ct+=1
+            new = slow.next
+            slow.next = prev 
+            prev = slow
+            slow = new
         if(fast.next!=None):
-            lt.append(slow.val)
-            ct+=1
-        while(slow.next!=None):
-                slow = slow.next
-                if(slow.val!=lt[ct-1]):
-                    return False
-                ct-=1
+            new = slow.next
+            slow.next = prev 
+            prev = slow
+            slow = new
+        else:
+            slow = slow.next
+        while(slow!= None):
+            if(slow.val!=prev.val):
+                return False
+            slow = slow.next
+            prev = prev.next
         return True
+
+        
             
             
