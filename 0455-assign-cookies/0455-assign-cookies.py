284979e7019decf9a166ecffaa19223a31461ddc
Shankar Jonnavaram <58230306+shankarjonnavram@users.noreply.github.com>
2023-08-10 11:12:46 +0530

    Time: 158 ms (83.02%), Space: 18.2 MB (72.65%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        child_sz = len(g)
+        cookie_sz = len(s)
+        ch = 0 
+        cook = 0
+        ct = 0
+        # while(cook<cookie_sz and ch < child_sz):
+        #     if(s[cook]>=g[ch]):
+        #         ct+=1
+        #         cook+=1
+        #         ch+=1
+        #     else:
+        #         break
+        for i in range(child_sz):
+            if(cook == cookie_sz):
+                break
+            while(cook<cookie_sz and s[cook]<g[i]):
+                cook+=1
+            if(cook<cookie_sz and s[cook]>=g[i]):
+                ct+=1
+                cook+=1
+        return ct
+    
