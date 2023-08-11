a88dd54190b18af6e40ffd497014ea279c03b652
Shankar Jonnavaram <58230306+shankarjonnavram@users.noreply.github.com>
2023-08-11 10:00:28 +0530

    Time: 410 ms (90.15%), Space: 16.5 MB (47.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,13 +3,17 @@ class Solution:
         sz = len(s)
         res = ""
         reslen = 0 
+        left = -1
+        right = -1
         for i in range(sz):
             # Odd Condition
             lt = i
             rt = i
             while(lt>=0 and rt<sz and s[lt]==s[rt]):
                 if(rt-lt+1 > reslen):
-                    res = s[lt:rt+1]
+                    # res = s[lt:rt+1]
+                    left = lt
+                    right = rt
                     reslen = rt-lt+1
                 lt-=1
                 rt+=1
@@ -17,11 +21,13 @@ class Solution:
             lt ,rt = i , i+1
             while(lt>=0 and rt<sz and s[lt]==s[rt]):
                 if(rt-lt+1 > reslen):
-                    res = s[lt:rt+1]
+                    # res = s[lt:rt+1]
+                    left = lt
+                    right = rt
                     reslen = rt-lt+1
                 lt-=1
                 rt+=1
-        return res
+        return s[left:right+1]
             
             
             
