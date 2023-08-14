95f6df22a830014c8b9d34de9ed7190055ac5ec0
Shankar Jonnavaram <58230306+shankarjonnavram@users.noreply.github.com>
2023-08-14 18:42:15 +0530

    Time: 54 ms (75.64%), Space: 55.4 MB (5.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,13 @@
-import re
+def st_reverse(i,lt,n):
+    if(i>=n//2):
+        return True
+    return lt[i]==lt[n-i-1] and st_reverse(i+1,lt,n)
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         st= ""
         for i in s:
             if (i.isalnum()):
                 st=st+i.lower()
-        return st==st[::-1]
+        return st_reverse(0,st,len(st))
+        
         
